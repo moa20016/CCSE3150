47c9937cb13e3f7db3e398d6ac4579595a8f4913
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-02-05 22:58:31 -0500

    Update well_balanced.h

--- Lab2/well_balanced.h
+++ Lab2/well_balanced.h
@@ -2,7 +2,10 @@
 #define __WELL_BALANCED_H
 
 bool isWellBalanced(const std::vector<int>& sequence);
-std::vector<int> generateWellBalancedList(int n);
+std::vector<int> generateRandomParentheses(int n);
 void fisherYatesShuffle(std::vector<int>& sequence);
+void swap(int* a, int* b);
+void showData(const std::vector<int>& array);
+void fisher_yates(int array[], const int length, int (* random_fcn)());
 
 #endif
