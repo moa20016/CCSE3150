ea0618d17fb202df264c76bad2dfa84b440ce22a
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-02-13 19:36:55 -0500

    Update linked_list.h

--- Lab3/linked_list.h
+++ Lab3/linked_list.h
@@ -1,12 +1,17 @@
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
-struct Node {
-    Node* next;
+struct node {
+    int data;
+    struct node* next;
 };
 
-void initializeLinkedList(Node* head);
-void pointerJumping(Node* head);
-void printLinkedList(Node* head);
+struct node * createLinkedList(int n);
+
+void pointerJumping(struct node * head);
+
+int getValue(struct node * start, int node_number, int total_elements);
+
+void print(struct node * start, int total_elements);
 
 #endif 
