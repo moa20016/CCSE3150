3a028802b07c247a1b5fd16ad0fbcb35ebb8669d
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-02-13 19:36:41 -0500

    Update linked_list.cpp

--- Lab3/linked_list.cpp
+++ Lab3/linked_list.cpp
@@ -1,34 +1,60 @@
 #include "linked_list.h"
 #include <iostream>
 
-void initializeLinkedList(Node* head) {
-    Node* current = head;
-    for (int i = 0; i < 4; ++i) { //length of linked list
-        current->next = new Node();
-        current = current->next;
+using namespace std;
+
+struct node * createLinkedList(int n) {
+    if (n <= 0) {
+        return NULL;
     }
-    current->next = current;
+
+    struct node * root = new node;
+    root->data = 1;
+    struct node * linked_list = root;
+
+    for (int i = 2; i <= n; i++) {
+        linked_list->next = new node;
+        linked_list = linked_list->next;
+        linked_list->data = i;
+    }
+
+    linked_list->next = root;
+
+    return root;
 }
 
-void pointerJumping(Node* head) {
-    Node* current = head;
-    Node* last = nullptr;
-    while (current->next != current) {
-        last = current->next;
-        current = current->next;
+void pointerJumping(node * head) {
+    node* curr = head;
+    while (curr->next != head) {
+        curr = curr->next;
     }
-    current = head;
-    while (current->next != current) {
-        current->next = last;
-        current = current->next;
+    curr->next = NULL;
+    node* end = curr;
+    curr = head;
+    while (curr != NULL) {
+        node* next = curr->next;
+        curr->next = end;
+        curr = next;
     }
 }
 
-void printLinkedList(Node* head) {
-    Node* current = head;
-    while (current->next != current) {
-        std::cout << current << " -> ";
-        current = current->next;
+int getValue(struct node * start, int node_number, int total_elements) {
+    if (node_number < 1 || node_number > total_elements) {
+        return -1;
     }
-    std::cout << current << std::endl;
+
+    struct node * linked_list = start;
+    for (int i = 1; i < node_number; i++) {
+        linked_list = linked_list->next;
+    }
+
+    return linked_list->data;
+}
+
+void print(struct node * start, int total_elements) {
+    struct node * linked_list = start;
+    for (int i = 0; i < total_elements; i++) {
+        std::cout << "Node " << i+1 << ": " << linked_list << std::endl;
+        linked_list = linked_list->next;
+    }
 }
