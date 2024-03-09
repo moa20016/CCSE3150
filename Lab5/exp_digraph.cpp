17466bfcc8634805ef5b346671fa545522d2690d
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-03-09 01:22:28 -0500

    Update exp_digraph.cpp

--- Lab5/exp_digraph.cpp
+++ Lab5/exp_digraph.cpp
@@ -11,13 +11,13 @@ void expensiveDigraphExactPaths(vector<vector<int>>& DNeg1, vector<vector<int>>&
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 for (int k = 0; k < n; ++k) {
-                    if (DZero[i][k] == 0 && DPos1[k][j] == 0) {
+                    if (((DNeg1[i][k] + DPos1[k][j]) == 0) || ((DPos1[i][k] + DNeg1[k][j]) == 0)) {
                         DZero[i][j] = 0;
                     }
-                    if (DPos1[i][k] == 1 && DPos1[k][j] == 1) {
+                    if (((DPos1[i][k] + DPos1[k][j]) == 1) || ((DZero[i][k] + DPos1[k][j]) == 1)) {
                         DPos1[i][j] = 1;
                     }
-                    if (DNeg1[i][k] == -1 && DZero[k][j] == -1) {
+                    if (((DNeg1[i][k] + DZero[k][j]) == -1) || ((DZero[i][k] + DNeg1[k][j]) == -1)) {
                         DNeg1[i][j] = -1;
                     }
                 }
