bad9136a243466b6c638a49771814ba803f8421b
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-03-15 23:39:38 -0400

    Update main.cpp

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include "prefixsumoperations.h"
+#include "prefixsumoperations.h" // Assuming this header contains the function declarations
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
     int n;
-    std::cout << "Enter the value of n: ";
+
+    std::cout << "Enter the size of the array: ";
     std::cin >> n;
 
     std::vector<int> result = createNonCrossingPrefixSum(n);
-
-    std::cout << "Non-crossing prefix sum array: ";
-    for (int num : result) {
-        std::cout << num << " ";
+    std::cout << "Non-crossing Prefix Sum Array: ";
+    for (size_t i = 0; i < result.size(); ++i) {
+        std::cout << result[i] << " ";
     }
     std::cout << std::endl;
 
