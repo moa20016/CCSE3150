7c96a67f380e8bd00692893c1377e49253a054f5
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-04-18 19:53:25 -0400

    Update Triangle3d.h

--- Week9/Triangle3d.h
+++ Week9/Triangle3d.h
@@ -19,12 +19,25 @@ private:
     Point3d p2;
     Point3d p3;
 
+    double s;
+    double d12;
+    double d13;
+    double d23;
+    double AreaIn3D;
+
 public:
     Point3d & get_p1() { return p1; }
     Point3d & get_p2() { return p2; }
     Point3d & get_p3() { return p3; }
 
+    double get_area() {return AreaIn3D;}
+    double get_HeronsS() {return s;}
+
     double pointDistance(const Point3d left, const Point3d right);
+
+
+    void heronsS();
+    void area();
 
 
 };
