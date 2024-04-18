7f4cbaa56bafcbbd7c21ae28d53f04d54b28a529
Mohammad Abujaffar <144379554+moa20016@users.noreply.github.com>
2024-04-18 19:56:58 -0400

    Update main.cpp

--- Week9/main.cpp
+++ Week9/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-
+#include <cmath>
 #include "Triangle3d.h"
 
 using namespace std;
 
 int main() {
-
     Point3d p1(0.0, 0.0, 0.0);
     Point3d p2(1.0, 2.0, 0.0);
     Point3d p3(-1.0, 0.0, 0.0);
 
+    Triangle3d triangle(p1, p2, p3);
+    cout << "Distance between p1 and p2: " << triangle.pointDistance(p1, p2) << endl;
+    cout << "Expected distance: sqrt(5) = " << sqrt(5.0) << endl;
 
-    Triangle3d triangle(p1,p2,p3);
-    cout << triangle.pointDistance(p1,p2) << endl;
-    cout << "That should have been the sqrt(5) = sqer(1^2 + 2^2)" << endl;
+    triangle.area();
+    cout << "Area of the triangle: " << triangle.get_area() << endl;
 
     return 0;
 }
